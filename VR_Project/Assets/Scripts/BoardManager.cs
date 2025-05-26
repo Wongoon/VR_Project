34adb2ad13c6b60e9f7a669c762bf3e175de3bfb
Wongoon <rmsgh64@gmail.com>
2025-05-27 00:06:53 +0900

    Update: `Game Scene`

---
+++ VR_Project/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    [SerializeField] private Transform Board;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Board.GetChild(0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
