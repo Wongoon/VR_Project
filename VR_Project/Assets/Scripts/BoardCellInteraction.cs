9a5e288b1cb9172130e9525b39a014536c36a647
Wongoon <93926767+Wongoon@users.noreply.github.com>
2025-05-28 10:47:35 +0900

    Update: `BoardCellInteraction.cs`
    승패 여부 코드 추가

--- VR_Project/Assets/Scripts/BoardCellInteraction.cs
+++ VR_Project/Assets/Scripts/BoardCellInteraction.cs
@@ -25,7 +25,7 @@ public class BoardCellInteraction : MonoBehaviour
 
     public void OnSelectEntered(SelectEnterEventArgs args) {
         if (placedStones.ContainsKey(coordinates)) {
-            Debug.Log("Return");
+            Debug.Log($"Board Clicked : {coordinates}");
             return;
         }
 
@@ -35,6 +35,43 @@ public class BoardCellInteraction : MonoBehaviour
         GameObject stone = Instantiate(prefab, spawnPos, Quaternion.identity);
         placedStones.Add(coordinates, stone);
 
+        if (CheckWin(coordiantes, isBlackTurn)) {
+            Debug.Log(isBlackTurn ? "Black Win" : "White Win");
+        }
+
         isBlackTurn = !isBlackTurn;
+    }
+
+    private bool CheckWin(Vector2Int, current, bool black) {
+        Vector2Int[] directions = new Vector2Int[] {
+            Vector2Int.right, Vector2Int.up, new Vector2Int(1, 1), new Vector2Int(1, -1)
+        };
+
+        foreach (Vector2Int dir in directions) {
+            int count = 1;
+            count += CountInDirection(current, dir, black);
+            count += CountInDirection(current, -dir, black);
+
+            if (count >= 5)
+                return true;
+        }
+        return false;
+    }
+
+    private int CountInDirection(Vector2Int start, Vector2Int dir, bool black) {
+        int count = 0;
+        Vector2Int pos = start + dir;
+
+        while (placedStones.TryGetValue(pos, out GameObject stone)) {
+            bool isBlackStone = stone.CompareTag("BlackStone");
+            bool isWhiteStone = stone.CompareTag("WhiteStone");
+
+            if ((black && isBlackstone) || (!black && isWhiteStone)) {
+                count++;
+                pos += dir;
+            }
+            else break;
+        }
+        return count;
     }
 }
